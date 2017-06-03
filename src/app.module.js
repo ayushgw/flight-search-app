f4095ca943c4ab86b931379131d67b95c175001b
ayushgosi <anime.ayush@gmail.com>
2017-06-04 04:01:40 +0530

    added animations to ui-views and ng-if transitions

--- src/app.module.js
+++ src/app.module.js
@@ -7,7 +7,7 @@ import uirouter from 'angular-ui-router';
 
 import './app.css';
 import Routes from './app.config';
-import EnableUIRouterErrors from './app.run';
+import RunFunction from './app.run';
 
 import main from './main/main.module';
 import home from './home/home.module';
@@ -18,4 +18,4 @@ import Data from './Data/Data.module';
 
 angular.module('app', [nganimate, uirouter, main, home, search, results, Data])
 .config(Routes)
-.run(EnableUIRouterErrors);
+.run(RunFunction);
